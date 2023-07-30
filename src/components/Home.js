f6c9d3c8a4353a67222d4dbe586e4434f1997cb8
Calvin Debrah <debrahblessed2000@gmail.com>
2023-07-30 09:01:06 +0000

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbabr from './Navbar'
+
+const Home = () => {
+  return (
+    <div className="home-container">
+ <Navbabr/>
+    </div>
+ 
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbabr = () => {
+  return (
+    <div>
+      
+      
+    </div>
+  )
+}
+
+export default Navbabr
